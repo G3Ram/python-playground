7b3801268552b3282251a372bf10540017788d4b
Gayathri Ramakrishnan <gayathri.t.ramakrishnan@gmail.com>
2024-06-14 09:37:35 -0500

    PCEP - var + operators

--- var-ground.py
+++ var-ground.py
@@ -13,3 +13,14 @@ a = 3.0
 b = 4.0
 c = (a ** 2 + b ** 2) ** 0.5
 print(c)
+
+# lab
+# var - round
+kilometers = 12.25
+miles = 7.38
+
+miles_to_kilometers = miles * 1.61
+kilometers_to_miles = kilometers / 1.61
+
+print(miles, "miles is", round(miles_to_kilometers, 2), "kilometers")
+print(kilometers, "kilometers is", round(kilometers_to_miles, 2), "miles")
