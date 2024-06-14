ce899afda64a8cb471bb2c6bc041c42fede8d195
Gayathri Ramakrishnan <gayathri.t.ramakrishnan@gmail.com>
2024-06-14 09:11:24 -0500

    PCEP - var + operators

--- var-ground.py
+++ var-ground.py
@@ -2,3 +2,14 @@
 # var - simple assignment
 var = 1
 print(var)
+
+# scenario 2 - success
+# var - print with +
+var = "3.8.5"
+print("Python version: " + var)
+
+# pythagorus theorem
+a = 3.0
+b = 4.0
+c = (a ** 2 + b ** 2) ** 0.5
+print(c)
