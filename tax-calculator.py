8bda14851f6a4f1ea4675bfec8aa3e3e0b361e04
Gayathri Ramakrishnan <gayathri.t.ramakrishnan@gmail.com>
2024-06-30 10:12:23 -0500

    tax calculator - calculate tax for more

--- tax-calculator.py
+++ tax-calculator.py
@@ -3,5 +3,8 @@ income = float(input("Enter your income: "))
 tax = 0
 if income < 85528:
     tax = (income * 18) / 100 - 556.2
+else:
+    surplus = (income - 85528) * 0.32
+    tax = 14839.2 + surplus
 
-print("Tax is ", round(tax))
+print("Tax is ", float(round(tax)), "thalers")
