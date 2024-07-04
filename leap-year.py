7c77746db056cb027554c568052921e0f7be5240
Gayathri Ramakrishnan <gayathri.t.ramakrishnan@gmail.com>
2024-07-04 10:21:59 -0500

    leap year - additional checks

--- leap-year.py
+++ leap-year.py
@@ -5,3 +5,7 @@ if year % 4 != 0:
     print("It's a common year")
 elif year % 100 != 0:
     print("It's a leap year")
+elif year % 400 != 0:
+    print("It's a common year")
+else:
+    print("It's a leap year")
