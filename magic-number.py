fba4904dfcc46c8623f3d442b89cc128e99c7145
Gayathri Ramakrishnan <gayathri.t.ramakrishnan@gmail.com>
2024-07-09 10:33:54 -0500

    magic number - first version

--- magic-number.py
+++ magic-number.py
@@ -1,1 +1,10 @@
 # Find the magic number
+secret_number = 777
+is_secret_number = False
+while not is_secret_number:
+    user_input = int(input("Guess the secret number: "))
+    if user_input == secret_number:
+        print("Well done, muggle! You are free now.")
+        is_secret_number = True
+    else:
+        print("Ha ha! You're stuck in my loop!")
