2120915ff7a0e64e8f1426c85f9a686403209247
Gayathri Ramakrishnan <gayathri.t.ramakrishnan@gmail.com>
2024-07-20 11:15:12 -0500

    eating vowels - improvize

--- vowels.py
+++ vowels.py
@@ -1,9 +1,11 @@
 #  Eating vowels
 word = input("Enter a word: ")
 word = word.upper()
-
+word_without_vowels = ""
 for i in word:
     if i in ["A", "E", "I", "O", "U"]:
         continue
     else:
-        print(i)
+        word_without_vowels += i
+
+print(word_without_vowels)
