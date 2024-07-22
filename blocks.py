79eb7978f87f04214517db9d11f13037147558b3
Gayathri Ramakrishnan <gayathri.t.ramakrishnan@gmail.com>
2024-07-22 12:11:03 -0500

    blocks - end

--- blocks.py
+++ blocks.py
@@ -1,1 +1,17 @@
 # Find the height of the blocks
+number_of_blocks = int(input("Enter number of blocks: "))
+height = []
+while number_of_blocks > 0:
+    if len(height) == 0:
+        height.append(1)
+        number_of_blocks -= 1
+    else:
+        for i in range(len(height)):
+            if number_of_blocks > 0:
+                height[i] += 1
+                number_of_blocks -= 1
+        if number_of_blocks > 0:
+            height.append(1)
+            number_of_blocks -= 1
+
+print(len(height))
