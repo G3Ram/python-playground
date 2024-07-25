27e880e4b769b9e4fdc94c145a79439304364e16
Gayathri Ramakrishnan <gayathri.t.ramakrishnan@gmail.com>
2024-07-25 12:20:58 -0500

    lothar - end

--- lothar.py
+++ lothar.py
@@ -1,1 +1,12 @@
 # Lothar hypothesis
+c0 = int(input("Enter a real number: "))
+steps = 0
+while c0 != 1:
+    steps += 1
+    if c0 % 2 == 0:
+        c0 //= 2
+    else:
+        c0 = 3 * c0 + 1
+    print(c0)
+
+print("Steps = ", steps)
