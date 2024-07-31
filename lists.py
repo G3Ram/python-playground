8b7c41941c19869a03adf47bcdd3cf85688f838c
Gayathri Ramakrishnan <gayathri.t.ramakrishnan@gmail.com>
2024-07-31 08:57:08 -0500

    lists - example 1

--- lists.py
+++ lists.py
@@ -1,1 +1,8 @@
 # Python lists
+
+# Example 1 - Let's assign a new value of 111 to the first element in the list. We do it this way:
+numbers = [10, 5, 7, 2, 1]
+print("Original list content:", numbers)  # Printing original list content.
+
+numbers[0] = 111
+print("New list content: ", numbers)  # Current list content.
