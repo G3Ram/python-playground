c451da3564cab54bbe1c4939a8d7e4da7d61b327
Gayathri Ramakrishnan <gayathri.t.ramakrishnan@gmail.com>
2024-08-13 09:57:29 -0500

    beatles step4

--- beatles.py
+++ beatles.py
@@ -14,4 +14,8 @@ for i in range(2):
     beatles.append(new_name)
 
 # step 4: use the del instruction to remove Stu Sutcliffe and Pete Best from the list;
+del beatles[4]
+del beatles[3]
+
+print(beatles)
 # step 5: use the insert() method to add Ringo Starr to the beginning of the list.
