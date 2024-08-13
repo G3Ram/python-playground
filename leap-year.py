2ec6759db521c8511e996fe11ad7780bf1d2eec6
Gayathri Ramakrishnan <gayathri.t.ramakrishnan@gmail.com>
2024-08-13 15:44:40 -0500

    leap year using function

--- leap-year.py
+++ leap-year.py
@@ -11,3 +11,17 @@ elif year % 400 != 0:
     print("It's a common year")
 else:
     print("It's a leap year")
+
+
+# is_leap_year using a function
+def is_leap_year(year):
+    if year < 1582:
+        return False
+    elif year % 4 != 0:
+        return False
+    elif year % 100 != 0:
+        return True
+    elif year % 400 != 0:
+        return False
+    else:
+        return True
