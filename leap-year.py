bdef16fed0b58107deb7e89dcd419c2a11f3eabf
Gayathri Ramakrishnan <gayathri.t.ramakrishnan@gmail.com>
2024-08-13 15:45:51 -0500

    leap year using function - test data

--- leap-year.py
+++ leap-year.py
@@ -25,3 +25,16 @@ def is_leap_year(year):
         return False
     else:
         return True
+
+
+# check function using test data
+test_data = [1900, 2000, 2016, 1987]
+test_results = [False, True, True, False]
+for i in range(len(test_data)):
+    yr = test_data[i]
+    print(yr, "->", end="")
+    result = is_leap_year(yr)
+    if result == test_results[i]:
+        print("OK")
+    else:
+        print("Failed")
