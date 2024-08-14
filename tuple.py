cbc10f62c2acf3113eb0af479d7c8d87ba264d82
Gayathri Ramakrishnan <gayathri.t.ramakrishnan@gmail.com>
2024-08-14 09:58:21 -0500

    tuple example

---
+++ tuple.py
@@ -0,0 +1,11 @@
+# tuple examples
+my_tuple = (1, 23, 450, 1000)
+
+print(my_tuple[0])
+print(my_tuple[-1])
+print(my_tuple[-1:])
+print(my_tuple[1:])
+print(my_tuple[1:2])
+
+for item in my_tuple:
+    print(item)
