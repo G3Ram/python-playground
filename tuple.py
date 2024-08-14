0a31e398d47a2f83677f4a74367cee8b7010f98f
Gayathri Ramakrishnan <gayathri.t.ramakrishnan@gmail.com>
2024-08-14 10:01:41 -0500

    tuple example 2

--- tuple.py
+++ tuple.py
@@ -9,3 +9,10 @@ print(my_tuple[1:2])
 
 for item in my_tuple:
     print(item)
+
+# Example 2
+tuple_1 = my_tuple * 2
+print(tuple_1)
+tuple_2 = my_tuple + (1000, 20000)
+print(tuple_2)
+print(1000 in my_tuple)
