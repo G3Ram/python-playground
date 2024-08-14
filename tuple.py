0632990fd8da57d585f92efeeb875c598e1bfbd1
Gayathri Ramakrishnan <gayathri.t.ramakrishnan@gmail.com>
2024-08-14 10:02:54 -0500

    tuple example 3

--- tuple.py
+++ tuple.py
@@ -16,3 +16,14 @@ print(tuple_1)
 tuple_2 = my_tuple + (1000, 20000)
 print(tuple_2)
 print(1000 in my_tuple)
+
+# Example 3
+var = 123
+
+t1 = (1,)
+t2 = (2,)
+t3 = (3, var)
+
+t1, t2, t3 = t2, t3, t1
+
+print("t1, t2, t3 is ", t1, t2, t3)
