e78ec1274681bd4a45d086560cf91b043143e15a
Gayathri Ramakrishnan <gayathri.t.ramakrishnan@gmail.com>
2024-08-15 06:18:12 -0500

    output check

--- test-output.py
+++ test-output.py
@@ -5,3 +5,7 @@ int("python")  # ValueError
 
 l = [1, 2, 3]
 l.index(5)  # ValueError
+
+# Note: if the step value is 0 Python raises a ValueError exception
+for i in range(0, 5, 0):  # ValueError
+    print(i)
