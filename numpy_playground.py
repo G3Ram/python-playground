d4f15baeaaaa24f1a9717872d2de0bd982802c94
Gayathri Ramakrishnan <gayathri.t.ramakrishnan@gmail.com>
2024-08-19 10:03:21 -0500

    feat: notebook basics

--- numpy_playground.py
+++ numpy_playground.py
@@ -3,3 +3,10 @@ import numpy as np
 # Single Dimensional array
 a_arr = np.array([1, 2, 3])
 print(a_arr)
+
+b_arr = np.arange(3)
+c_arr = np.arange(0, 10, 2)
+d_arr = np.arange(0, 10, 3, dtype=float)
+print(b_arr)
+print(c_arr)
+print(d_arr)
